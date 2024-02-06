bae0b2493f328c6c93d4dae779a3cf0b69941429
omeatai <omeatai@gmail.com>
2024-02-05 22:41:42 -0700

    3. Advanced Static Props
    3. Advanced Static Props

--- app-1/src/App.tsx
+++ app-1/src/App.tsx
@@ -2,6 +2,7 @@ import "./App.css";
 import { Greet } from "./components/Greet";
 import { Person } from "./components/Person";
 import { PersonList } from "./components/PersonList";
+import { Status } from "./components/Status";
 
 function App() {
   const personName = {
@@ -29,6 +30,7 @@ function App() {
       <Greet name="Ifeanyi" messageCount={20} isLoggedIn={false} />
       <Person name={personName} />
       <PersonList names={nameList} />
+      <Status status="loading" />
     </div>
   );
 }
