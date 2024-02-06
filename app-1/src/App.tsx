da80eec7f0515480c6a6fd8db942407104f491fa
omeatai <omeatai@gmail.com>
2024-02-05 23:34:33 -0700

    6. Event Props
    6. Event Props

--- app-1/src/App.tsx
+++ app-1/src/App.tsx
@@ -1,41 +1,21 @@
 import "./App.css";
-import { Greet } from "./components/Greet";
-import { Person } from "./components/Person";
-import { PersonList } from "./components/PersonList";
-import { Status } from "./components/Status";
-import { Oscar } from "./components/Oscar";
-import { Heading } from "./components/Heading";
+import { Button } from "./components/Button";
+import { Input } from "./components/Input";
 
 function App() {
-  const personName = {
-    first: "Bruce",
-    last: "Wayne",
-  };
-
-  const nameList = [
-    {
-      first: "Bruce",
-      last: "Wayne",
-    },
-    {
-      first: "Clark",
-      last: "Kent",
-    },
-    {
-      first: "Princess",
-      last: "Diana",
-    },
-  ];
-
   return (
     <div className="App">
-      <Greet name="Ifeanyi" messageCount={20} isLoggedIn={false} />
-      <Person name={personName} />
-      <PersonList names={nameList} />
-      <Status status="loading" />
-      <Oscar>
-        <Heading> Oscar goes to the children component</Heading>
-      </Oscar>
+      <Button
+        handleClick={() => {
+          console.log("Button clicked");
+        }}
+      />
+      <Button
+        handleClick={(event, id) => {
+          console.log("Button Event clicked", event, id);
+        }}
+      />
+      <Input value="" handleChange={(event) => console.log(event)} />
     </div>
   );
 }
---
+++ app-1/src/components/Button.tsx
@@ -0,0 +1,16 @@
+type ButtonProps = {
+  handleClick?: (
+    event: React.MouseEvent<HTMLButtonElement>,
+    id: number
+  ) => void;
+};
+
+export const Button = (props: ButtonProps) => {
+  return (
+    <button
+      onClick={(event) => props.handleClick && props.handleClick(event, 2)}
+    >
+      Click Event
+    </button>
+  );
+};
