f157db0a27109e046e4cf1acd1eb82758e35f92e
omeatai <omeatai@gmail.com>
2024-02-08 14:37:31 -0700

    3. Stateful Class Component
    3. Stateful Class Component

--- ce-react/src/App.js
+++ ce-react/src/App.js
@@ -1,10 +1,10 @@
 import "./App.css";
-import Greet from "./components/Greet";
+import GreetClass from "./components/GreetClass";
 
 function App() {
   return (
     <div className="App">
-      <Greet />
+      <GreetClass />
     </div>
   );
 }
---
+++ ce-react/src/components/GreetClass.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class GreetClass extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello Ifeanyi to Class Component!</h1>
+      </div>
+    );
+  }
+}
+
+export default GreetClass;
