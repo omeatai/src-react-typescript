2ec5ffcdc9d9d8f748738e6a91a2571e02339ae2
omeatai <omeatai@gmail.com>
2024-02-19 12:44:06 -0700

    changes

--- ce-react/src/App.js
+++ ce-react/src/App.js
@@ -1,13 +1,9 @@
 import "./App.css";
-import { UserProvider } from "./contexts/userContext";
-import ComponentC from "./components/ComponentC";
 
 function App() {
   return (
     <div className="App">
-      <UserProvider value="Vishwas">
-        <ComponentC />
-      </UserProvider>
+      <h1>App</h1>
     </div>
   );
 }
