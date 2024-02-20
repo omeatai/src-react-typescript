1dfbd70da58cc05282ca49cf38c6adee59114ce9
omeatai <omeatai@gmail.com>
2024-02-19 19:42:52 -0700

    25. React HTTP - GET
    25. React HTTP - GET

--- ce-react/src/App.js
+++ ce-react/src/App.js
@@ -1,9 +1,10 @@
 import "./App.css";
+import PostList from "./components/PostList";
 
 function App() {
   return (
     <div className="App">
-      <h1>App</h1>
+      <PostList />
     </div>
   );
 }
